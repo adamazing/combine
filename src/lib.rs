84cdcc5f6eca2c44808d77536d17756b2087af1e
Adam Henley <adamazing@gmail.com>
2022-08-28 01:11:22 +1200

    Feature Load Level From Level File
    Signed-off-by: Adam Henley <adamazing@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, render::camera::{DepthCalculation, ScalingMode, WindowOrigin}, window::WindowMode};
+use bevy_ecs_ldtk::prelude::*;
 
 /** Global game constants */
 pub const CLEAR: Color = Color::rgb(0.2,0.1,0.1);
@@ -13,32 +14,67 @@ use debug::DebugPlugin;
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Copy)]
 pub enum GameState {
     MainMenu,
-    Playing,
+    GameLoading,
+    GamePlaying,
     GameOver,
-    PauseMenu,
+    GamePaused,
 }
 
 pub fn app() -> App {
     let mut app = App::new();
     app
     .add_state(GameState::MainMenu)
+    .add_plugins(DefaultPlugins)
+    .add_plugin(LdtkPlugin)
+    .add_plugin(DebugPlugin)
+    .insert_resource(LevelSelection::Index(0))
     .insert_resource(ClearColor(CLEAR))
     .insert_resource(WindowDescriptor {
         title: LAUNCHER_TITLE.to_string(),
         canvas: Some("#bevy".to_string()),
         fit_canvas_to_parent: true,
+        resizable: false,
+
+        mode: WindowMode::Fullscreen,
         ..Default::default()
     })
-    .add_plugins(DefaultPlugins)
-    .add_plugin(DebugPlugin)
-    .add_startup_system(load_icon);
+    // .register_ldtk_entity::<MyBundle>("MyEntityIdentifier")
+    .add_startup_system(setup);
     app
 }
 
-fn load_icon(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn_bundle(Camera2dBundle::default());
-    commands.spawn_bundle(SpriteBundle {
-        texture: asset_server.load("bevy.png"),
-        ..default()
+fn setup(mut commands:Commands, asset_server: Res<AssetServer>) {
+    let mut camera_bundle = commands.spawn_bundle(Camera2dBundle{
+       projection: OrthographicProjection{
+        depth_calculation: DepthCalculation::ZDifference,
+        scaling_mode: ScalingMode::WindowSize,
+        window_origin: WindowOrigin::BottomLeft,
+        ..Default::default()
+       },
+       ..Default::default()
+    });
+
+
+    camera_bundle.log_components();
+    // camera_bundle. = OrthographicProjection {
+    //     far,
+    //     depth_calculation: DepthCalculation::ZDifference,
+    //     scaling_mode: ScalingMode::WindowSize,
+    //     window_origin: WindowOrigin::BottomLeft,
+    //     ..Default::default()
+    // };
+
+    commands.spawn_bundle(LdtkWorldBundle {
+        ldtk_handle: asset_server.load("levels/level01.ldtk"),
+        ..Default::default()
     });
 }
+
+// #[derive(Bundle, LdtkEntity)]
+// pub struct MyBundle {
+//     a: ComponentA,
+//     b: ComponentB,
+//     #[sprite_sheet_bundle]
+//     #[bundle]
+//     sprite_bundle: SpriteSheetBundle,
+// }
